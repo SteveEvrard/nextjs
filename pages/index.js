dde4cc9e7e1ef60c5c78f15a5473806ff1a39a71
Stephen Evrard <stephenevrard@stephens-air.home>
2021-01-18 20:23:33 -0500

    test

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Steve's new app!</a>
+          Welcome to <a href="https://nextjs.org">Steve's app!</a>
         </h1>
 
         <p className={styles.description}>
