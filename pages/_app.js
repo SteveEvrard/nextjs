5789d79f74ed38b512a30b8f7193ce403415f5c4
Stephen Evrard <stephenevrard@stephens-air.home>
2021-01-18 20:51:30 -0500

    add polaris elements

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,9 @@ import React from "react";
 
 import App from 'next/app';
 import Head from 'next/head';
+import { AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/dist/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
 
 class MyApp extends App {
   render() {
@@ -13,7 +16,9 @@ class MyApp extends App {
           <title>Sample App</title>
           <meta charSet="utf-8" />
         </Head>
-        <Component {...pageProps} />
+        <AppProvider i18n={translations}>
+          <Component {...pageProps} />
+        </AppProvider>
       </React.Fragment>
     );
   }
