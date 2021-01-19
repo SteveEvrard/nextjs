32c54083897947efd4b2b502968f26521afe7981
Stephen Evrard <stephenevrard@stephens-air.home>
2021-01-18 20:09:57 -0500

    add config and update home

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import App from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <React.Fragment>
+        <Head>
+          <title>Sample App</title>
+          <meta charSet="utf-8" />
+        </Head>
+        <Component {...pageProps} />
+      </React.Fragment>
+    );
+  }
 }
 
-export default MyApp
+export default MyApp;
